5272eb23b71a69f641c91d01f2c23a3e817d90bf
Raghav Gupta <raghav@gpp.co.in>
2020-04-21 13:06:21 +0530

    Asking for tea

--- Intro/file01.js
+++ Intro/file01.js
@@ -3,6 +3,8 @@ function greet(name)
 	console.log('knock knock knock ' + name)
 	console.log('knock knock knock ' + name)
 	console.log('knock knock knock ' + name)
+	console.log('Nice to have you here')
+	console.log('Would you like some tree?')
 }
 
 greet('Raghav')
